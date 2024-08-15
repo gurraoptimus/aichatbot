f528a9d2b5482b7a03bc275a08e6a5dc3ff35dc2
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-08-15 13:11:21 +0000

    A.I ChatBot

--- ai.cpp
+++ ai.cpp
@@ -22,7 +22,9 @@ int main(){
         transform(user_input.begin(),user_input.end(),user_input.begin(),::tolower);
         
         if(user_input=="hi"||user_input=="hello"||user_input=="hey"){
-            cout<<"Hello user"
-        }       
+            cout<<"Hello user, how can i help you?"<<endl;
+        }else if(user_input=="how are you"||user_input=="how are you ok"){
+            cout<<"As a chatbot i have no emotional states"<<endl;
+        }else if(user_input==)
     }
 }
